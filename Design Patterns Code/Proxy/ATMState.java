9ec6c24991c41971b6c8832a4d5406d301dc9f25
Jai Bhambri <jaibhambri2891@gmail.com>
2022-10-06 18:51:25 +0530

    update

---
+++ Design Patterns Code/Proxy/ATMState.java
@@ -0,0 +1,13 @@
+package Proxy;
+
+public interface ATMState {
+
+    void insertCard();
+
+    void ejectCard();
+
+    void insertPin(int pinEntered);
+
+    void requestCash(int cashToWithdraw);
+
+}
